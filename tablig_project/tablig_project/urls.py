313a62b58eb6d4bdc1727c93bae8132d9f373f0d
mdzihad42 <zihadhossain.dev@gmail.com>
2026-04-16 14:58:32 +0600

    Fix URL conflicts for Musolli list

--- tablig_project/tablig_project/urls.py
+++ tablig_project/tablig_project/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('members/', include('tablig.urls')),
-    path('', include('tablig.urls')), # Set as default for now
+    path('', include('tablig.urls')),
 ]
